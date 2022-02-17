f7df2195045b2f7b30466b2b9f7d4307d5467c47
wanncye <wanncye@wanncyedeMacBook-Air.local>
2022-02-17 22:18:44 +0800

    斐波那契数列

--- TimeComplexity.cpp
+++ TimeComplexity.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+/*
+chrono是一个头文件的名字，但是也是一个std下的一个子命名空间的名字：
+在这个头文件下的所有元素(除了(不包含)common_type 类 特殊化)不是
+直接定义在std命名空间下的(就像大多数标准库一样)而是定义在std::chrono
+命名空间里。
+*/
 #include <chrono>
 #include <thread>
 using namespace std;
@@ -38,12 +44,13 @@ int main() {
     while (1) {
         cout << "输入n：";
         cin >> n;
+        //system_clock 提供访问当前的time_point,是一个全系统实时时钟。
         milliseconds start_time = duration_cast<milliseconds >(
             system_clock::now().time_since_epoch()
         );
         function1(n);
-//        function2(n);
-//        function3(n);
+        // function2(n);
+        // function3(n);
         milliseconds end_time = duration_cast<milliseconds >(
             system_clock::now().time_since_epoch()
         );
@@ -51,3 +58,9 @@ int main() {
             <<" ms"<< endl;
     }
 }
+/*
+结论：
+O(nlogn)算法1s时间内能够计算的量级为10^7
+O(n)算法1s时间内能够计算的量级为10^8
+O(n^2)算法1s时间内能够计算的量级为10^4
+*/
