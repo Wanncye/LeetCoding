9f3e61975b177dd5825dcaaab33db14e4315607b
wanncye <wanncye@wanncyedeMacBook-Air.local>
2022-02-18 09:50:46 +0800

    添加README

--- test.cpp
+++ test.cpp
@@ -4,50 +4,16 @@
 using namespace std;
 using namespace chrono;
 
-// O(n)
-void function1(long long n) {
-    long long k = 0;
-    for (long long i = 0; i < n; i++) {
-        k++;
-    }
-}
-
-// O(n^2)
-void function2(long long n) {
-    long long k = 0;
-    for (long long i = 0; i < n; i++) {
-        for (long j = 0; j < n; j++) {
-            k++;
-        }
-    }
-
-}
-
-// O(nlogn)
-void function3(long long n) {
-    long long k = 0;
-    for (long long i = 0; i < n; i++) {
-        for (long long j = 1; j < n; j = j*2) { // 注意这里j=1
-            k++;
-        }
-    }
-}
-
+struct node{
+   int num;
+   char cha;
+}st;
 int main() {
-    long long n; // 数据规模
-    while (1) {
-        cout << "输入n：";
-        cin >> n;
-        milliseconds start_time = duration_cast<milliseconds >(
-            system_clock::now().time_since_epoch()
-        );
-        function1(n);
-//        function2(n);
-//        function3(n);
-        milliseconds end_time = duration_cast<milliseconds >(
-            system_clock::now().time_since_epoch()
-        );
-        cout << "耗时:" << milliseconds(end_time).count() - milliseconds(start_time).count()
-            <<" ms"<< endl;
-    }
+    int a[100];
+    char b[100];
+    cout << sizeof(int) << endl;
+    cout << sizeof(char) << endl;
+    cout << sizeof(a) << endl;
+    cout << sizeof(b) << endl;
+    cout << sizeof(st) << endl;
 }
