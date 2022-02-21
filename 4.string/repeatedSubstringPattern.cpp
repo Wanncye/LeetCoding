a9be4470f854d7f9157630c9ccba2f4140da20c3
wanncye <535593370@qq.com>
2022-02-21 16:47:24 +0800

    重复的子字符串

--- 4.string/repeatedSubstringPattern.cpp
+++ 4.string/repeatedSubstringPattern.cpp
@@ -5,13 +5,34 @@
 using namespace std;
 class Solution {
 public:
-    bool repeatedSubstringPattern(string s) {
-
+    void getNext (int* next, const string& s){
+        next[0] = -1;
+        int j = -1;
+        for(int i = 1;i < s.size(); i++){
+            while(j >= 0 && s[i] != s[j+1]) {
+                j = next[j];
+            }
+            if(s[i] == s[j+1]) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+    bool repeatedSubstringPattern (string s) {
+        if (s.size() == 0) {
+            return false;
+        }
+        int next[s.size()];
+        getNext(next, s);
+        int len = s.size();
+        if (next[len - 1] != -1 && len % (len - (next[len - 1] + 1)) == 0) {
+            return true;
+        }
+        return false;
     }
 };
-
 int main(){
-    string str = "abab";
+    string str = "abcdabcd";
     Solution stl;
     cout<<stl.repeatedSubstringPattern(str)<<endl;
 }
