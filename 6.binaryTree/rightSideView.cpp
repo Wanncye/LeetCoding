e658d2a99118c6d306357eb1681b2dc7f607a03f
wanncye <535593370@qq.com>
2022-02-23 11:15:54 +0800

    二叉树的右视图

---
+++ 6.binaryTree/rightSideView.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        queue<TreeNode*> treeQueue;
+        if(!root) return result;
+        treeQueue.push(root);
+        while(!treeQueue.empty()){
+            vector<int> subResult;
+            int size = treeQueue.size();
+            for(int i=0; i<size; i++){
+                TreeNode* tmp = treeQueue.front();
+                treeQueue.pop();
+                subResult.push_back(tmp->val);
+                if(tmp->left) treeQueue.push(tmp->left);
+                if(tmp->right) treeQueue.push(tmp->right);
+            }
+            for(int i:subResult) cout<<i<<" ";
+            cout<<endl;
+            result.push_back(subResult.back());
+        }
+        for(int i:result) cout<<i<<" ";
+        cout<<endl;
+        return result;
+    }
+};
+
+int main(){
+    TreeNode *root = new TreeNode(3);
+    TreeNode *node1 = new TreeNode(9);
+    TreeNode *node2 = new TreeNode(20);
+    TreeNode *node3 = new TreeNode(15);
+    TreeNode *node4 = new TreeNode(7);
+    root->right = node2;
+    root->left = node1;
+    node2->left = node3;
+    node2->right = node4;
+    Solution stl;
+    stl.rightSideView(root);
+}
