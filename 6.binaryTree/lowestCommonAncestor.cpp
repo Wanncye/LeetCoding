aa82de6fd9aedf072b4d5ff7b3566e119dfc0e52
wanncye <535593370@qq.com>
2022-02-24 17:02:41 +0800

    二叉树的最近公共祖先：后序遍历

---
+++ 6.binaryTree/lowestCommonAncestor.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<cmath>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == p || root == q || root ==NULL) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left != NULL && right != NULL) return root;
+
+        if (left == NULL && right != NULL) return right;
+        else if (left != NULL && right == NULL) return left;
+        else  { //  (left == NULL && right == NULL)
+            return NULL;
+        }
+    }
+};
+int main(){
+    TreeNode *root = new TreeNode(3);
+    TreeNode *node1 = new TreeNode(9);
+    TreeNode *node2 = new TreeNode(20);
+    TreeNode *node3 = new TreeNode(15);
+    TreeNode *node4 = new TreeNode(7);
+    root->left = node1;
+    root->right = node2;
+    node1->left = node3;
+    node2->right = node4;
+    Solution stl;
+    stl.countNodes(root, node1, node2)<<endl;;
+}
