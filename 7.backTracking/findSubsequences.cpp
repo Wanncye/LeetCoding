a8ec466ce791612aa9abc98efe0c116b0278af62
wanncye <535593370@qq.com>
2022-02-27 12:21:15 +0800

    递增子序列

---
+++ 7.backTracking/findSubsequences.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<unordered_set>
+using namespace std;
+
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(vector<int>& nums, int startIndex) {
+        if (path.size() > 1) {
+            result.push_back(path);
+        }
+        unordered_set<int> uset; // 使用set来对本层元素进行去重
+        for (int i = startIndex; i < nums.size(); i++) {
+            if ((!path.empty() && nums[i] < path.back())
+                    || uset.find(nums[i]) != uset.end()) {
+                    continue;
+            }
+            uset.insert(nums[i]); // 记录这个元素在本层用过了，本层后面不能再用了
+            path.push_back(nums[i]);
+            backtracking(nums, i + 1);
+            path.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        result.clear();
+        path.clear();
+        backtracking(nums, 0);
+        return result;
+    }
+};
+
+int main(){
+    vector<int> nums = {1,2,1,1};
+    Solution stl;
+    stl.findSubsequences(nums);
+}
