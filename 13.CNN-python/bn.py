d04abfd5644fc820d6558bb711fa123c782efa9c
wanncye <535593370@qq.com>
2022-03-29 13:38:40 +0800

    完善BN后向传播

--- 13.CNN-python/bn.py
+++ 13.CNN-python/bn.py
@@ -1,9 +1,8 @@
 # --coding:utf-8--
 import numpy as np
 
-
-
-
+# https://blog.csdn.net/qq_27370437/article/details/119569007
+# https://zhuanlan.zhihu.com/p/100672008
 
 class MyBN:
     def __init__(self, momentum, eps, num_features):
@@ -30,27 +29,27 @@ class MyBN:
         :param x: 数据
         :return: BN输出
         """
-        x_mean = x.mean(axis=0)
-        x_var = x.var(axis=0)
+        self.x_mean = x.mean(axis=0)
+        self.x_var = x.var(axis=0)
         # 对应running_mean的更新公式
-        self._running_mean = (1-self._momentum)*x_mean + self._momentum*self._running_mean
-        self._running_var = (1-self._momentum)*x_var + self._momentum*self._running_var
+        self._running_mean = (1-self._momentum)*self.x_mean + self._momentum*self._running_mean
+        self._running_var = (1-self._momentum)*self.x_var + self._momentum*self._running_var
         # 对应论文中计算BN的公式
-        x_hat = (x-x_mean)/np.sqrt(x_var+self._eps)
-        self.y = self._gamma*x_hat + self._beta
+        self.x_hat = (x-self.x_mean)/np.sqrt(self.x_var+self._eps)
+        self.y = self._gamma*self.x_hat + self._beta
         return self.y
 
-    # def backward(self, dout, cache):
-    #     '''
-    #     dout:前一层传过来的导数
-    #     '''
-    # 	xhat, self._gamma, ivar = cache
-            
-    #     dbeta = np.sum(dout, dim=0)
-    #     dgamma = np.sum(dout * xhat, dim=0)
+    def backward(self,dout):
+        '''
+        dout:前一层传过来的导数
+        '''
+        print(dout.shape)
+        print(self.x_hat.shape)
+        self.dgamma = np.sum(dout * self.x_hat, axis=0)
+        self.dbeta = np.sum(dout, axis=0)
         
-    #     dy = dout * gamma
-    #     dx = ivar * (dy - np.mean(dy, dim=0) - xhat * np.mean(dy * xhat, dim=0))
+        dy = dout * self._gamma
+        dx = 1/self.x_var * (dy - np.mean(dy, axis=0) - self.x_hat * np.mean(dy * self.x_hat, axis=0))
 
 data = np.array([[1, 2],
                  [1, 3],
@@ -58,3 +57,5 @@ data = np.array([[1, 2],
 my_bn = MyBN(momentum=0.01, eps=0.001, num_features=2)
 bn_output = my_bn.batch_norm(data, )
 print(bn_output)
+dout = np.random.random((3,2))
+my_bn.backward(dout)
