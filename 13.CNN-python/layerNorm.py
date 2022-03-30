ae2826a2e7f6d26a9976314983c2e295a34c5b28
wanncye <535593370@qq.com>
2022-03-30 15:01:57 +0800

    layer Norm

---
+++ 13.CNN-python/layerNorm.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def Layernorm(x, gamma, beta):
+
+    # x_shape:[B, C, H, W]
+    results = 0.
+    eps = 1e-5
+
+    x_mean = np.mean(x, axis=(1, 2, 3), keepdims=True)
+    x_var = np.var(x, axis=(1, 2, 3), keepdims=True)
+    x_normalized = (x - x_mean) / np.sqrt(x_var + eps)
+    results = gamma * x_normalized + beta
+    return results
