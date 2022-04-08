e30d1ae9ff0bb60726120d41d4c0f11e1ed417d0
wanncye <535593370@qq.com>
2022-04-08 16:14:01 +0800

    最长有效括号，使用栈

--- 0.other/longestValidParentheses.cpp
+++ 0.other/longestValidParentheses.cpp
@@ -32,31 +32,55 @@ using namespace std;
 // };
 
 // dp
+// class Solution {
+// public:
+//     int longestValidParentheses(string s) {
+//         int maxLen = 0;
+//         int n = s.size();
+//         vector<int> dp(n, 0);
+//         for(int i=1; i<n; i++){
+//             if(s[i] == ')'){
+//                 if(s[i-1] == '('){
+//                     dp[i] = (i >= 2 ? dp[i-2] : 0) + 2;
+//                 }else if(i - dp[i-1] > 0 && s[i - dp[i-1] - 1] == '('){
+//                     dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+//                 }
+//                 cout<<dp[i]<<endl;
+//                 maxLen = max(maxLen, dp[i]);
+//             }
+            
+//         }
+//         cout << maxLen << endl;
+//         return maxLen;
+//     }
+// };
+
+// stack
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int maxLen = 0;
-        int n = s.size();
-        vector<int> dp(n, 0);
-        for(int i=1; i<n; i++){
-            if(s[i] == ')'){
-                if(s[i-1] == '('){
-                    dp[i] = (i >= 2 ? dp[i-2] : 0) + 2;
-                }else if(i - dp[i-1] > 0 && s[i - dp[i-1] - 1] == '('){
-                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+        int maxans = 0;
+        stack<int> stk;
+        stk.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                stk.push(i);
+            } else {
+                stk.pop();
+                if (stk.empty()) {
+                    stk.push(i);
+                } else {
+                    maxans = max(maxans, i - stk.top());
                 }
-                cout<<dp[i]<<endl;
-                maxLen = max(maxLen, dp[i]);
             }
-            
         }
-        cout << maxLen << endl;
-        return maxLen;
+        return maxans;
     }
 };
 
+
 int main(){
-    string str = "()(())";
+    string str = "(()(())";
     Solution stl;
     stl.longestValidParentheses(str);
 }
