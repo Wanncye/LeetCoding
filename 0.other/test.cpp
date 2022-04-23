1035ccd132bb485774e5f9445228ca9348f1d192
wanncye <535593370@qq.com>
2022-04-23 20:17:06 +0800

    4.23美团笔试

--- 0.other/test.cpp
+++ 0.other/test.cpp
@@ -16,19 +16,20 @@ bool cmp2(int a, int b){
 }
 
 int main(){
-    priority_queue<int, vector<int>, cmp> pq;
-    pq.push(4);
-    pq.push(3);
-    pq.push(5);
-    pq.push(7);
-    while(pq.empty()!=true){
-        cout<<pq.top()<<endl;
-        pq.pop();
-    }
+    // priority_queue<int, vector<int>, cmp> pq;
+    // pq.push(4);
+    // pq.push(3);
+    // pq.push(5);
+    // pq.push(7);
+    // while(pq.empty()!=true){
+    //     cout<<pq.top()<<endl;
+    //     pq.pop();
+    // }
 
-    vector<int> mySortArray={2,6,8,1,9};
-    sort(mySortArray.begin(), mySortArray.end(), cmp2);
-    for(int i : mySortArray) cout<<i<<" ";
-    cout<<endl;
+    // vector<int> mySortArray={2,6,8,1,9};
+    // sort(mySortArray.begin(), mySortArray.end(), cmp2);
+    // for(int i : mySortArray) cout<<i<<" ";
+    // cout<<endl;
+    cout<<(11>>1)<<endl;
 
 }
