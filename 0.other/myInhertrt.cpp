b341f0d11561930762b7dba8d58d86c08a9a21c1
wanncye <535593370@qq.com>
2022-05-03 13:26:53 +0800

    虚函数表

--- 0.other/myInhertrt.cpp
+++ 0.other/myInhertrt.cpp
@@ -1,24 +1,29 @@
 #include<iostream>
 using namespace std;
 
-
 class A{
 public:
-    // virtual void method(){
-    void method(){
+    virtual void method(){
+    // void method(){
         cout<<"in class A"<<endl;
+    }
+    void other(){
+        cout<<"in class A, other fun"<<endl;
     }
 };
 
 class B : public A{
 public:
-    B
     void method(){
         cout<<"in class B"<<endl;
+    }
+    void other(){
+        cout<<"in class B, other fun"<<endl;
     }
 };
 
 int main(){
     A * obj = new B();
     obj->method();
+    obj->other();
 }
