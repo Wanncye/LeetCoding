bc0b50f32be6ee159440ca272374ec66a1ef5848
wanncye <535593370@qq.com>
2022-05-12 12:32:33 +0800

    格雷编码二进制输出

--- 0.other/grayCode.cpp
+++ 0.other/grayCode.cpp
@@ -1,20 +1,30 @@
 #include<iostream>
 #include<vector>
+
+#include<bitset>
 using namespace std;
 
 class Solution {
 public:
+    // vector<int> grayCode(int n) {
+    //     vector<int> ret;
+    //     ret.reserve(1 << n);
+    //     // cout<<ret.size()<<endl;
+    //     // cout<<(1 << n)<<endl;
+    //     ret.push_back(0);
+    //     for (int i = 1; i <= n; i++) {
+    //         int m = ret.size();
+    //         for (int j = m - 1; j >= 0; j--) {
+    //             ret.push_back(ret[j] | (1 << (i - 1)));
+    //         }
+    //     }
+    //     return ret;
+    // }
     vector<int> grayCode(int n) {
-        vector<int> ret;
-        ret.reserve(1 << n);
-        // cout<<ret.size()<<endl;
-        // cout<<(1 << n)<<endl;
-        ret.push_back(0);
-        for (int i = 1; i <= n; i++) {
-            int m = ret.size();
-            for (int j = m - 1; j >= 0; j--) {
-                ret.push_back(ret[j] | (1 << (i - 1)));
-            }
+        vector<int> ret(1 << n);
+        for (int i = 0; i < ret.size(); i++) {
+            ret[i] = (i >> 1) ^ i;
+            cout << bitset<4>(ret[i]) << endl;
         }
         return ret;
     }
