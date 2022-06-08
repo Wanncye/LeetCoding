2740aa9cbe1ef3dc2c53687be5d360fd1a0a3e35
wanncye <535593370@qq.com>
2022-06-09 00:52:43 +0800

    lambada表达式mutable

--- 18.候捷/variadicTemplates.cpp
+++ 18.候捷/variadicTemplates.cpp
@@ -12,10 +12,30 @@ void printX(const T& firstArg, const Types&... args){
 	printX(args...);
 }
 
+void wcy(int, int){
+    cout << "in wcy(int, int), no para obj" << endl;
+}
+
 int main(){
-    int a = 2;
-    int b = 2;
-    int c = 2;
-    double d = 2;
-    printX(a, b, c, d);
+    // int a = 2;
+    // int b = 2;
+    // int c = 2;
+    // double d = 2;
+    // printX(a, b, c, d);
+    // wcy(1, 2);
+
+    // string *a = "wcy";
+    // cout << sizeof(nullptr) <<endl;
+
+
+    int id = 0;
+    auto f = [id] () {
+        cout << "id:" << id <<endl;
+        ++id;
+    };
+    id = 42;
+    f();
+    f();
+    f();
+    cout << "id:" << id <<endl;
 }
